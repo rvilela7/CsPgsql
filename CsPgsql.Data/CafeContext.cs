af55b2ee15fbc157533af973df7d6296844d2977
Rui Vilela <ruivil@gmail.com>
2022-05-17 11:51:12 +0100

    DbContext

---
+++ CsPgsql.Data/CafeContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CsPgsql.Data
+{
+    public class CafeContext : DbContext
+    {
+        public DbSet<Cafe> Cafes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost:5432;Database=Cafe;Username=ruivilela;Password=pwd");
+    }
+
+    public class Cafe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
