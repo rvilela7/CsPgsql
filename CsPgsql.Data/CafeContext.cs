90d7e63eb3ae8c06ab579f6bd96f36ee9e334e64
Rui Vilela <ruivil@gmail.com>
2022-05-17 11:53:29 +0100

    todo

--- CsPgsql.Data/CafeContext.cs
+++ CsPgsql.Data/CafeContext.cs
@@ -8,7 +8,7 @@ namespace CsPgsql.Data
         public DbSet<Cafe> Cafes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql("Host=localhost:5432;Database=Cafe;Username=ruivilela;Password=pwd");
+            => optionsBuilder.UseNpgsql("Host=localhost:5432;Database=Cafe;Username=ruivilela;Password=pwd"); // Todo: upd pwd
     }
 
     public class Cafe
